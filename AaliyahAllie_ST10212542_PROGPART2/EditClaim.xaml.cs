af4fd14c78d13e8d15b8d0c298880e6bd3bd55a0
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-10-22 23:10:18 +0200

    Edit Claim added

---
+++ AaliyahAllie_ST10212542_PROGPART2/EditClaim.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AaliyahAllie_ST10212542_PROGPART2
+{
+    /// <summary>
+    /// Interaction logic for EditClaim.xaml
+    /// </summary>
+    public partial class EditClaim : Window
+    {
+        public EditClaim()
+        {
+            InitializeComponent();
+        }
+    }
+}
